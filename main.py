c19714ef701e3c185aa9d21409891e5c7da223a0
thoigianvang <lamya282859@gmail.com>
2026-06-17 09:50:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -894,7 +894,7 @@ def main():
         manual_test(events, state)
 
     elif MODE == "news":
-        gold_news_update(state)
+    gold_news_update(state, force=True)
 
     elif MODE == "bias":
         daily_gold_bias(events, state)
